e483cda6cd49eb60044c94abd3b48f50c564f552
NAPOLEON039 <nihar.raote@gmail.com>
2018-08-10 18:24:58 +0530

    Added redirect URI

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
+const hbs = require('hbs');
 const authRoutes = require('./routes/auth-routes');
-const p = require('./config/passport-setup');
+const setup = require('./config/passport-setup');
 const passport = require('passport');
 
 let app = express();
-app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'hbs');
 // app.use(passport.initialize())
 // app.use(passport.session())
 
 app.use(authRoutes);
 
 app.get('/', (req, res) => {
-    res.render('index.html');
+    res.render('index.hbs');
 });
-app.get('/signin', passport.authenticate('twitter'));
-app.get('/redirect', passport.authenticate('twitter', { failureRedirect: '/' }), (req, res) => {
-    res.redirect('/success');
-});
+// app.get('/redirect', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
+//     res.redirect('/success');
+// });
 
-app.use(serveStatic(__dirname + "/dist"));
+//app.use(serveStatic(__dirname + "/dist"));
 // app.use(session({
 //     secret: 'whatever',
 //     resave: true,
 //     saveUninitialized: true
 // }));
 
-let port = process.env.PORT || 5000;
-app.listen(port);
-console.log(`Server started at port ${port}`);
+app.listen(5000, () => {
+    console.log('Server started at port 5000');
+});
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,12 +1,18 @@
 const router = require('express').Router();
+const passport = require('passport');
 
 router.get('/success', (req, res) => {
-    res.render('Success.html')
+    res.render('success.hbs')
 })
-router.get('/signin', (req, res) => {
-    //handling with passport
-    res.send('Authenticating with Google...');
-});
+router.get('/signin', passport.authenticate('google', {
+    //what information to get from the user
+    scope: ['profile']
+}));
+
+//callback route for google to redirect to
+router.get('/redirect', (req, res) => {
+    res.send('You reached the callback URI');
+})
 
 router.get('/logout', (req, res) => {
     //Handling with passport
